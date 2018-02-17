31a769b2afe752198732ba7137b9d6bc23655d74
Chris <ctm665@gmail.com>
2018-02-16 18:46:31 -0800

    markavailability and decidedate js implementation (except for next and back button functionality). Locationselect still needs dropdown searchlist.

---
+++ javascript/DecideDate.js
@@ -0,0 +1,110 @@
+
+var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+var daysInWeek = ['SUN','MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+var start = [1, 4, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6];
+
+var days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+var currentMonth = 1;
+var currdays = [];
+
+var chngMonth = function(mon){
+    var newMonth = mon;
+    var bM = newMonth-1;
+    var nM = newMonth+1;
+    if(newMonth < 0){
+        newMonth = 11;
+        bM = 10;
+        nM = 0;
+    }
+    else if(newMonth == 0){
+        bM = 11;
+    }
+    else if(newMonth == 11){
+        nM = 0;
+    }
+    else if(newMonth > 11){
+        newMonth = 0;
+        bM = 11;
+        nM = 1;
+    }
+    currentMonth = newMonth;
+    var monthselect = document.getElementById("month-select");
+    monthselect.innerHTML = '<h1><button type="button" onclick="chngMonth('+bM+')">'+months[bM]+'</button>'+months[newMonth]+'<button type="button" onclick="chngMonth('+nM+')">'+months[nM]+'</button></h1>';
+    makeCalendar();
+};
+
+var makeCalendar = function(){
+    var startday = start[currentMonth];
+    var line = [];
+    var i;
+    clearCalendar();
+    for(i=0; i<days[currentMonth]; i+=1){
+        var day = startday+i;
+        var curr = document.getElementById('d'+day);
+        curr.innerHTML = i+1;
+    }
+    for(i=0; i<currdays.length ; i+=1){
+        var translate = translateMonthDay(currentMonth, currdays[i]);
+        if(translate > 0){
+            var day = startday+translate;
+            var curr = document.getElementById('d'+day);
+            curr.style.backgroundColor = "gray";
+        }
+    }
+};
+
+var clearCalendar = function(){
+    var i;
+    for(i=0; i<42; i+=1){
+        var clear = document.getElementById('d'+i);
+        clear.innerHTML = '';
+        clear.style.backgroundColor = "white";
+    }
+};
+
+var selectDay = function(day){
+    var date = day-start[currentMonth];
+    if(date < 0 || date >= days[currentMonth]) return false;
+    var cell = document.getElementById('d'+day);
+    var i = 0;
+    var select = 0;
+    while(i < currentMonth){
+        select += days[i];
+        i+=1;
+    }
+    select += date;
+    var contains = currdays.indexOf(select);
+    if(contains == -1) {
+        currdays.push(select);
+        cell.style.backgroundColor = "gray";
+        console.log("added day: "+select);
+    }
+    else{
+        currdays.splice(contains,1);
+        cell.style.backgroundColor = "white";
+        console.log("removed day: "+select);
+    }
+    return true;
+};
+
+var translateMonthDay = function(month, yearday){
+    var range = 0;
+    var i =0;
+    while(i<currentMonth){
+        range += days[i];
+        i+=1;
+    }
+    var bound = range+days[i];
+    if(yearday < range || yearday > bound) return -1;
+    return yearday-range;
+};
+
+var initialMonth = function(){
+    chngMonth(currentMonth);
+};
+
+window.onload = function(){
+    initialMonth();
+};
