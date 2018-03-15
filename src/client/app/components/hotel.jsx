8f39608141c27800c4cf8fa68aee9e08c7c6f4d5
dlt009 <davidsinovercos@gmail.com>
2018-03-15 14:55:15 -0700

    Updating styles for hotel page

--- src/client/app/components/hotel.jsx
+++ src/client/app/components/hotel.jsx
@@ -3,39 +3,58 @@ import ProgressButtons from './progress_bottom_bar.jsx';
 import CommentComponent from './comments.jsx';
 import ChatContainer from './agent_chat.jsx';
 
-class HotelContainer extends React.Component {
-    constructor(props) {
-        super(props);
+const Hotel = (props) => {
 
-        this.handleClick = this.handleClick.bind(this);
+    let hotelStyles = {
+        border: '1px solid lightgray',
+        margin: '10px',
+        padding: '10px',
+        boxShadow: '0px 3px 3px #888888',
     }
 
-    handleClick(event) {
+    let titleStyles = {
+        fontWeight: 'bold',
+        margin: '10px',
+    }
+
+    let cityPicStyles = {
+
+    }
+
+    render(
+        <div style={eventContainerStyles}>
+            <div className='row'>
+                <div style={titleStyles}>
+                    <h2>Location Title</h2>
+                </div>
+                <div style={cityPicStyles}>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+class HotelList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+    
+    handleOnClick(event) {
         console.log("Redirecting to AirBnB");
         window.location = "https://www.airbnb.com/";
     }
 
     render() {
         return(
-                <div id="outer-container">
-                    <br></br>
-                    
-                    <p>While visiting Chiang Mai, keep in mind that the old town
-                        district and area surrounding the famous night market offers both expensive and
-                        inexpensive options. The riverside neighborhoods boast exclusive hotels whereas
-                        the inner cities allow you to live like a local.</p>
-                        <div id="hotel-container">
-                            <img id="hotel-logo" src="/imgs/airbnb-logo-s.png" alt="hotel-logo"/>
-                                <h4>Live like a local and find unique places to stay</h4>
-                                <p>from 02/02/19<br></br> to 02/07/19</p>
-                                <p>
-                                    <button id="hotel-button" type="button" onClick={this.handleClick}>Check Out Homes on AirBnB</button>
-                                </p>
-                        </div>
+            <div>
+                <div className={`${styles['hotel-container']}`}>
+
                 </div>
-            
+            </div>
         );
-    }    
+    }
 }
 
 const HotelPage = (props) => {
@@ -58,20 +77,28 @@ const HotelPage = (props) => {
         backgroundColor: 'dodgerblue'
     };
 
+    let mainContainerStyles = {
+        margin: 'auto',
+        border: '1px solid black',
+        padding: '10px',
+        width: '80%',
+        borderRadius: '25px',
+        marginBottom: '20px'
+    };
+
     return(
         <div>
             <div style={progressBarContainerStyle}><div style={progressBarContentStyle}></div></div>
-            <div id="main-container">
+            <div style={mainContainerStyles}>
 
-                <div id="top-prompt">
+                <div className={`${styles['top-prompt']}`}>
                     <h1>Where does everyone want to stay?</h1>
-                    <h4>Vote on where you want to stay!<br></br>
-                    Do you want to relax at the best hotel or live like a local?</h4>
+                    <h4>Vote on where you want to stay!</h4>
                 </div>
-                <HotelContainer/>
+                <HotelList/>
                 <CommentComponent/>
+                <ChatContainer/>
             </div>
-            <ChatContainer/>
             <ProgressButtons backRoute={backRouteRef} nextRoute={nextRouteRef} />
         </div>
     );
